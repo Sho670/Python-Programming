6ca974a15aab6cb05f61e15ed36f4363965d3c09
Sho670 <shohom2005@gmail.com>
2026-01-24 18:52:38 +0530

    Update Display number of occurences.py

--- Display number of occurences.py
+++ Display number of occurences.py
@@ -1,6 +1,8 @@
 # This program shows to read a multi digit number and provide the fequency of every particular number.
+
 # this is a very simple way to put the concept into the code of python.
 n=input("Enter a number:")
+
 print("The number is:",n)
 D=set(n)
 for ele in D:
