f8b509c9ada3aeb1e72b6e2fea6b1b31c5cc5069
Sho670 <shohom2005@gmail.com>
2026-01-24 18:51:00 +0530

    Frequency of words.py
    The following program denoted displaying the top 10 most frequent words in a particular text file. 
    
    The first step is to create a file with a particular name, and save it to the local repository for convivence.  Also, add a paragraph of content into the file to get the information.
    
    In the program, during the input of the file name, the same file should be included into the filename.
    
    Following some of the basic commands may help the user to work with the code much conveniently.

---
+++ Frequency of words.py
@@ -0,0 +1,45 @@
+# This program display's the frequency of the words in a file.
+# As of now you can print the 10 most frequent words from it.
+
+import sys/*-q
+import string
+import os.path
+
+name=input("Enter the file name saved:")
+
+if not os.path.isfile(name):
+  print("File",name,"doesn't exists")
+  sys.exit(0)
+
+ifile=open(name,"r")
+filecont=""
+
+for sentence in ifile:
+  for ch in line:
+    if ch not in string.punctuation:
+      filecont=filecont + ch
+    else:
+      filecont=filecont + ''
+  
+  wordF = {}
+  wordList = filecont.split()
+
+
+#Calculating the Frequency of the particular words
+
+for word in wordList:
+  if word not in wordF.keys():
+    wordF[word] = 1
+  else:
+    wordF[word] += 1
+
+sortWord = sorted(wordF.items(),key = lambda x:x[1], reverse= True)
+
+# Now we have to display the most frequent words
+#Final step
+
+print("Most Frequent words are:")
+for i in range(10):
+  print(sortWord[i][0], "occurs", sortWord[i][1], "times")
+
+print("End of Output..........")
