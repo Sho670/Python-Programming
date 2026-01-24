9860f60cd5d32eff25ba6ceb51a47dd0076a4787
Sho670 <shohom2005@gmail.com>
2026-01-24 18:55:44 +0530

    Update Function for calculating factorial of a number.py
    This program provides an insight on calculating the factorial of an number by providing the input number from the user side.
    
    Also, it provides a binomial coefficient of the same number provided from the user side.

--- Function for calculating factorial of a number.py
+++ Function for calculating factorial of a number.py
@@ -1,11 +1,18 @@
 # This is a sample program to calculate factorial of a number using any number.
+
 # Also this program is designed to compute a binomial coefficient as well.
+
 def fact(num):
   if num==0:
     return 1
+    
   else:
     return num*fact(num-1)
+    
 a=int(input("Enter value of N:"))
+
 b=int(input("Enter value of b, which should not be negative :"))
+
 aCb=fact(a)/(fact(b)*fact(a-b))
+
 print(a,'C',r,"=","d"%aCb,sep="")
