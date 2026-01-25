4e135e5e6662dddbfd9bc5cd5e56602e1efef633
Shohom <181641785+Sho670@users.noreply.github.com>
2026-01-25 19:38:40 +0530

    Election Eligibility.py
    This program provides the information about whether the particular person is eligible for voting in election.

---
+++ Election Eligibility.py
@@ -0,0 +1,12 @@
+# This is sample program to check for the person, as the person is eligible for the election for voting or not.
+
+# The age eligibility for a person to vote is above 18 Years
+
+a=int(input("Enter the age of the person:"))
+if (a=>18):
+  print("The person is eligible for voting.")
+  print("As the persons age is:",a)
+else:
+  print("The person is not eligible for voting now.")
+
+print("End of Discussion......")
