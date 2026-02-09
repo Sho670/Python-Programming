51c682e77b056721efe21eb0e6a6c50d668b54d6
Shohom <181641785+Sho670@users.noreply.github.com>
2026-02-09 18:38:36 +0530

    Update Even & Odd.py

--- Even & Odd.py
+++ Even & Odd.py
@@ -1,2 +1,9 @@
 # The program includes a input from a user, of only one number.
+
 # Henceforth, we will be able to decide whether the number is odd or even.
+
+n1=float(input("enter the number"))
+if n1 %2==0:
+  print("The number is even number.")
+else:
+  print("The number is odd number.")
