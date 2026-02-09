0568c07c03d6f23505b10137cc26f463833715bc
Shohom <181641785+Sho670@users.noreply.github.com>
2026-02-09 18:41:08 +0530

    Update Even & Odd.py

--- Even & Odd.py
+++ Even & Odd.py
@@ -1,10 +1,13 @@
+# this is a very easy execution program with a single condition
 # The program includes a input from a user, of only one number.
 
 # Henceforth, we will be able to decide whether the number is odd or even.
 
 # The code contains nothing but a simple condition which is whether the number is module of 2 or not.
 
+
 n1=float(input("enter the number"))
+
 if n1 %2==0:
   print("The number is even number.")
 else:
