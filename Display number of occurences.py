3f9146f1b83ba18296231fa511f47c9afa29512a
Shohom <181641785+Sho670@users.noreply.github.com>
2026-02-09 18:22:57 +0530

    Update Display number of occurences.py

--- Display number of occurences.py
+++ Display number of occurences.py
@@ -3,9 +3,11 @@
 # This is a very simple way to put the concept into the code of python.
 
 # Here we are using "count" , which means that how many times the particular element is being repeated in the number.
+
 n=input("Enter a number:")
 
 print("The number is:",n)
 D=set(n)
+
 for ele in D:
   print(ele,"occurs",n.count(ele),"times")
