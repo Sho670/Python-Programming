0136a10a3b2f1fa45254fcc897b077022270554b
Shohom <181641785+Sho670@users.noreply.github.com>
2026-02-09 18:39:33 +0530

    Update Even & Odd.py

--- Even & Odd.py
+++ Even & Odd.py
@@ -2,6 +2,8 @@
 
 # Henceforth, we will be able to decide whether the number is odd or even.
 
+# The code contains nothing but a simple condition which is whether the number is module of 2 or not.
+
 n1=float(input("enter the number"))
 if n1 %2==0:
   print("The number is even number.")
