0663d4cc7953801d886b5deb84119ab14027c6c5
Shohom <181641785+Sho670@users.noreply.github.com>
2026-02-09 18:20:03 +0530

    Update Finding Largest Number.py
    Same description  from the last commit.

--- Finding Largest Number.py
+++ Finding Largest Number.py
@@ -1,5 +1,5 @@
 # This is a sample program to find the largest number between the two numbers.
-
+# The program can also be able to compare three numbers as well, all it requires is providing and declaring three numbers in the input.
 a=int(input("Enter the first number"))
 
 b=int(input("Enter the second number"))
