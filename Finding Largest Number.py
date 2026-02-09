b17927f63e5363fb232a1c84d9ca108a1af8d887
Shohom <181641785+Sho670@users.noreply.github.com>
2026-02-09 18:17:47 +0530

    Create Finding Largest Number.py
    By declaring two inputs from the user, we can be able to compare two numbers integers and display which number is the largest.

---
+++ Finding Largest Number.py
@@ -0,0 +1,10 @@
+# This is a sample program to find the largest number between the two numbers.
+
+a=int(input("Enter the first number"))
+
+b=int(input("Enter the second number"))
+
+if a>b:
+  print("The largest number is :",a)
+else:
+  print("The largest number is :",b)
