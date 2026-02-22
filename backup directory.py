9268c0e0f3f61976bae1d4115f40bb19d4d3b81a
Shohom <181641785+Sho670@users.noreply.github.com>
2026-02-22 19:49:26 +0530

    Update backup directory.py

--- backup directory.py
+++ backup directory.py
@@ -12,11 +12,14 @@ directory = input("Enter Directory name that have to be backup:")
 
 if not os.path.isdir (directory):
   print("Directory", directory, "does not exists")
+  
   sys.exit(0)
 
 
 currentDirectory = pathlib.Path(directory)
+
 with zipfile.ZipFile("myZip.zip", node = "w") as archive:
+  
   for file_path in currentDirectory.rglob("*"):
     archive.write(file_path,arcname= file_path.relative_to(currentDirectory))
   if os.path.isfile("myZip.zip"):
