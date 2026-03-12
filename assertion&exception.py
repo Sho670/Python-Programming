f23dc2d85cafee591aa7b2e2c7ed87a082512014
Shohom <181641785+Sho670@users.noreply.github.com>
2026-03-12 19:23:44 +0530

    Update assertion&exception.py
    This program shows a glimpse of how the assertion and Exception is being used as the function DivExp as a Demo in the Code.

--- assertion&exception.py
+++ assertion&exception.py
@@ -4,18 +4,22 @@
 import sys
 def DivExp (a,b):
   assert a>0, "a should be greater than 0"
+  
   try:
     c=a/b
+    
   except ZeroDivisionError:
     print("Value of b cannot be zero")
     sys.exit(0)
+    
   else:
     return c
-value1= int(input("Enter the value of a:"))
-value2= int(input("Enter the value of b:"))
-value3= DivExp(value1, value2)
+    
+value1 = int(input("Enter the value of a:"))
+value2 = int(input("Enter the value of b:"))
+value3 = DivExp(value1, value2)
 
-print(value1, "/", value2, "=", value3)
+print (value1, "/", value2, "=", value3)
 
 
 # The Output for the code will be:
