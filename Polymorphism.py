9701ca42275f2097b0611e798d7c2dda907ba73b
Shohom <181641785+Sho670@users.noreply.github.com>
2026-03-27 19:35:11 +0530

    Update Polymorphism.py

--- Polymorphism.py
+++ Polymorphism.py
@@ -1,1 +1,14 @@
+# This code shows runtime polymorphism in python programming using method called sound() method.
 
+# This includes class Animal and various animals included with thier sounds.
+
+class Animal:
+  def sound(show):
+    return "Sound"
+class Cat(Animal):
+  def sound(show):
+    return "Meow"
+
+class Dog(Animal):
+  def sound(show):
+    return "Bark"
