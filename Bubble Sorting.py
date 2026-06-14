85c41dc6607bf69e71484c9c9f7b3bb461b7c28f
Shohom <181641785+Sho670@users.noreply.github.com>
2026-06-14 20:59:49 +0530

    Update Bubble Sorting.py

--- Bubble Sorting.py
+++ Bubble Sorting.py
@@ -11,3 +11,14 @@ def bubble(arr):
 
     for j in range(0,n-i-1):
       if arr[j]>arr[j+1]:
+        arr[j],arr[j+1]=arr[j+1],arr[j]
+
+        swap=True
+
+    if  not swap:
+      break
+
+
+return arr
+
+
