9575b262f2fc374295d23ceffbd554d32ccc823f
Shohom <181641785+Sho670@users.noreply.github.com>
2026-06-14 20:57:52 +0530

    Update Bubble Sorting.py

--- Bubble Sorting.py
+++ Bubble Sorting.py
@@ -1,2 +1,13 @@
 # Bubble Sorting In Python Programming
 
+def bubble(arr):
+
+
+  num=len(arr)
+
+  for i in range(num):
+
+    swap=False
+
+    for j in range(0,n-i-1):
+      if arr[j]>arr[j+1]:
