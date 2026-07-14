3f8218be96ce4dbb8eb126203a862356543bc324
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-14 10:30:05 +0530

    Update Missing Number.py

--- Missing Number.py
+++ Missing Number.py
@@ -5,3 +5,17 @@ class Solution:
   for i in range(0,l+1):
     if i not in nums:
       return i
+
+
+# Time Complexity: O(n^2)
+
+#Another method. Optimized
+
+l=len(nums)
+nsum= l*(l+1)//2
+
+s=sum(nums)
+
+return nsum-s
+
+# Time Complexity : O(n)
