b10315ef9ac47ec1fa93c8657c99c8f70f2aa033
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-14 10:16:19 +0530

    Update Majority Element.py

--- Majority Element.py
+++ Majority Element.py
@@ -4,3 +4,18 @@
 
 # Formula [n]/2
 
+a=[1,1,2,2,3,3,1]
+
+d={}
+for i in a :
+    if i in d:
+        d[i]=d[i]+1
+    else:
+        d[i]=1
+for i in d:
+  if d[i]>=len(a)/2:
+    
+    print(i,"-",d[i])
+
+
+# Keeping the same code as leetcode question 132
