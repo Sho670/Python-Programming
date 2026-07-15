2fa75d8fab08d0d97246cb87d1b5f085e30395ef
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-15 12:05:06 +0530

    Update Leetcode 152.py

--- Leetcode 152.py
+++ Leetcode 152.py
@@ -2,26 +2,27 @@
 
 def maximumproductsubarray(self, list -> int):
 
-h=1
-l=1
-ah=1
-al=1
+  def subarray():
 
-
+    h=1
+    l=1
+    ah=1
+    al=1
 
 m= max(nums)
 
-for i in nums:
-  h = i*ah
-  l = i*al
-  ah = max(h,l,i)
-  al = min(h,l,i)
+    for i in nums:
+      
+      h = i*ah
+      l = i*al
+      ah = max(h,l,i)
+      al = min(h,l,i)
 
 
-  if m < ah:
-    m = ah
+      if m < ah:
+        m = ah
 
 
-  return m
+    return m
 
   
