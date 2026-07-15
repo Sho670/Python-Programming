318ee80fce7a14d91d4b3dfebb7da9e1988dfa15
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-15 11:50:27 +0530

    Update Leetcode 560.py

--- Leetcode 560.py
+++ Leetcode 560.py
@@ -1,3 +1,14 @@
 # The question contains the functions to execute the dictionary and hash table using python.
 
 
+def subarraysumequal(self, list[int]):
+  sum=0
+  d= {0:1}
+  c=0
+
+  for i in nums:
+    sum +=i
+    left= sum-k
+
+    if
+  
