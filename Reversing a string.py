d8770038905e004346443cf1165417f89f5fab2f
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-20 14:32:12 +0530

    Update Reversing a string.py

--- Reversing a string.py
+++ Reversing a string.py
@@ -3,8 +3,12 @@
 def reversestring(self,num):
   def f(left,right):
     if (left>=right):
+      
       return
       s[left], s[right] = s[right], s[left]
 
       f(left +1, right-1)
+
+      
+      f(0,len(s)-1)
   
