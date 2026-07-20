f1f5ef1bafd7d652e2e88893d1845afbc9e6af47
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-20 14:31:40 +0530

    Update Reversing a string.py

--- Reversing a string.py
+++ Reversing a string.py
@@ -1,4 +1,10 @@
 # Leetcode 344 (Reversing String)
 
 def reversestring(self,num):
+  def f(left,right):
+    if (left>=right):
+      return
+      s[left], s[right] = s[right], s[left]
+
+      f(left +1, right-1)
   
