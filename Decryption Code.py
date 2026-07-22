c977ed972bc33230f27bef67f825c8d511ba23d9
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-22 10:32:50 +0530

    Update Decryption Code.py

--- Decryption Code.py
+++ Decryption Code.py
@@ -1,1 +1,14 @@
 # Here We are performing the decryption of the previous code i did for the encryption of the string.
+
+
+t= input("Enter text:")
+
+s= int(input("Enter shift:"))
+
+for ch in encrypted:
+
+  if ch.isalpha():
+    decrypted += chr(ord(ch)-s)
+
+  else:
+    decrypted += ch
