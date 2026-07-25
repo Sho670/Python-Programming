53af75ed0e8e4b47bd93dca7226f3ddfe72e02ef
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-25 18:28:12 +0530

    Update Two Pointer Approach.py

--- Two Pointer Approach.py
+++ Two Pointer Approach.py
@@ -2,3 +2,14 @@
 
 # Approach: Two Pointer Approach
 
+arr = [15,3,2,1,6,11,8]
+
+arrlen = len(arr)
+
+maxprofit=0
+minvalue=arr[0]
+
+for i in range(arrlen):
+  if arr[i] < minvalue:
+    minvalue = arr[i]
+    
